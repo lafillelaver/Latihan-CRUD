417b816fb1ab5f456ed631608c41974f67fed2d6
Dirahyu <149054544+lafillelaver@users.noreply.github.com>
2024-01-27 08:45:03 +0700

    Add files via upload

---
+++ db_latihan.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 27, 2024 at 02:37 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_latihan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_sample`
+--
+
+CREATE TABLE `tbl_sample` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(250) NOT NULL,
+  `last_name` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_sample`
+--
+
+INSERT INTO `tbl_sample` (`id`, `first_name`, `last_name`) VALUES
+(1, 'Muhammad', 'Zulfan'),
+(2, 'Ahmad', 'Albar'),
+(3, 'Fendi', 'Wibowo'),
+(5, 'Safa', 'Resty'),
+(6, 'Rizka', 'Fatikarani'),
+(7, 'Huang', 'Renjun'),
+(8, 'Haikal', 'Saputra'),
+(10, 'Nur Syifa', 'Salsabila');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_sample`
+--
+ALTER TABLE `tbl_sample`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_sample`
+--
+ALTER TABLE `tbl_sample`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
